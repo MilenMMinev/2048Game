13672393021ef83d0f28e7f06f63d03ca4c01bd9
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-07-10 19:20:14 +0300

    initialise class Number

--- src/com/hackbulgaria/game/Number.java
+++ src/com/hackbulgaria/game/Number.java
@@ -1,5 +1,21 @@
 package com.hackbulgaria.game;
 
 public class Number {
+	private int value;
 
+	Number() {
+		this.value = 0;
+	}
+
+	public void merge() {
+		this.value *= 2;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
