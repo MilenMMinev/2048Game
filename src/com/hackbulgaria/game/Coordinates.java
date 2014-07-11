7be8472473b102aefed3722621c5135c6bced320
Milen <milengminev@gmail.com>
2014-07-11 19:38:23 +0300

    Added a new helping class

---
+++ src/com/hackbulgaria/game/Coordinates.java
@@ -0,0 +1,31 @@
+package com.hackbulgaria.game;
+
+public class Coordinates {
+	private int x;
+	private int y;
+	
+	public Coordinates(){
+		this.x = -1;
+		this.y = -1;
+	}
+	
+	public Coordinates (int _x, int _y){
+		
+		x=  _x;
+		y = _y;
+	}
+	
+	public boolean compareTo(Coordinates other)
+	{
+		return this.x == other.x && this.y == other.y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+
+}
